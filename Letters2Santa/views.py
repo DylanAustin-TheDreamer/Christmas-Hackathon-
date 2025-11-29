d3df0a21dc287752a7ff58f08521436509aa77fc
Dylan Austin <austindylan0@gmail.com>
2025-11-29 13:04:56 +0000

    Set up django all-auth pages and redirects to home page. added bootstrap and set up navigation links.

--- Letters2Santa/views.py
+++ Letters2Santa/views.py
@@ -1,5 +1,35 @@
 from django.shortcuts import render
 
 # Create your views here.
+# of these views here are just basic pageloading
 def home(request):
     return render(request, 'home.html')
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
+
+
+# May use these again if we decide to style all-auth pages
+# def login(request):
+#     return render(request, 'account/login.html')
+
+# def signup(request):
+#     return render(request, 'account/signup.html')
+
+# def logout(request):
+#     return render(request, 'account/logout.html')
+
+# def change_password(request):
+#     return render(request, 'account/password_change.html')
+
+# def reset_password(request):
+#     return render(request, 'account/password_reset.html')
+
+# def complete_reset_password(request):
+#     return render(request, 'account/password_reset_done.html')
+
+# def email_management(request):
+#     return render(request, 'account/email.html')
+
+# def email_confirmation(request):
+#     return render(request, 'account/email_confirm.html')
