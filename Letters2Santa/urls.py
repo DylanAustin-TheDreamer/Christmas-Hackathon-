5627cf41d5629f65a3232be792431a371ce9f91c
Dylan Austin <austindylan0@gmail.com>
2025-11-29 20:40:47 +0000

    transfered data from database to front-end. AI responses are working.

--- Letters2Santa/urls.py
+++ Letters2Santa/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/', include('allauth.urls')),
-    path("write-letter/", views.write_letter, name="write_letter"), 
+    path("write-letter/", views.write_letter, name="write_letter"),
+    path('send-letter/', views.send_letter, name="send_letter"),
 ]
