c60c740157fb6eb973fdeae334115b3a2300bfef
Dylan Austin <austindylan0@gmail.com>
2025-11-29 19:37:05 +0000

    Updated calendar views and added some JS

--- adventcalendar/views.py
+++ adventcalendar/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-
+from .models import AdventDay
 # Create your views here.
 # advent calendar view function
 def calendar_view(request):
-    return render(request, 'adventcalendar/calendar.html')
+
+    try:
+        advent_day = AdventDay.objects.filter(user=request.user)
+    except:
+        return render(request, 'adventcalendar/calendar.html')
+    return render(request, 'adventcalendar/calendar.html', {'advent_day': advent_day})
