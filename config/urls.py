de94146e6514aa6ecb361c5ce9a2c47ce663686f
Christian <cxbeale@gmail.com>
2025-11-29 13:50:49 +0000

    add calendar app with views, urls, template basic

--- config/urls.py
+++ config/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Letters2Santa.urls')),
+    path('adventcalendar/', include('adventcalendar.urls')),
 ]
