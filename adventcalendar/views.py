f7e5716d9ac6d3a8e4d57ebf34c8e9d1440dcd28
Christian <cxbeale@gmail.com>
2025-11-30 12:09:57 +0000

    Set up Advent Calendar grid and styles

--- adventcalendar/views.py
+++ adventcalendar/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from datetime import datetime
 
 # Create your views here.
 # advent calendar view function
 def calendar_view(request):
-    return render(request, 'adventcalendar/calendar.html')
+    days = range(1, 25)  # creates a list from 1 to 24
+    today = datetime.now().day  # gets the current day of the month
+    return render(request, 'adventcalendar/calendar.html', {'days': days})
