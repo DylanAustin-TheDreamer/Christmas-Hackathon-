12d1b92a90ea4f05f1671870a443f0058a3f6905
Dylan Austin <austindylan0@gmail.com>
2025-12-01 11:37:37 +0000

    debug set to false for try deploy

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-umywb$l6pfyh76x81g5a707d$9k7hcbjz24ae2picx1$&0^1ks'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", '127.0.0.1', 'mychristmasapp.herokuapp.com']
 
